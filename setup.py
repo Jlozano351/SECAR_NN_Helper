b6d027a4bd91cfebc99ed36839794a9833485963
Jlozano351 <131318937+Jlozano351@users.noreply.github.com>
2024-04-12 00:56:50 -0400

    uploading the setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='SECAR_NN_helper',
+    version='1.0.0',
+    description='Library for the Neural Network model for the FRIB',
+    author='Juan José Lozano González',
+    author_email='lozanoju@msu.edu',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'torch',
+        'seaborn',
+        'matplotlib',
+        'pandas',
+    ],
+)
